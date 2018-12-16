a57a6d67bc20824eed14c1478f7f636ae660fd35
Daniel Friedrich <danielfriedrich1978@gmail.com>
2018-12-16 20:26:18 +0100

    Accordion ready

--- SNSFFW/js/SNSFFW.js
+++ SNSFFW/js/SNSFFW.js
@@ -1,14 +1,49 @@
-﻿/* 
- * SLIDE and SCROLL - FFW
- * https://www.slide-and-scroll.org/
- *
- * Copyright 2018 SLIDE and SCROLL by Daniel Friedrich
- * Released under the MIT license
- *
- * @author: Daniel Friedrich
- * Date: 01.08.2018
- */
+﻿/*
+SLIDE and SCROLL - FFW
+https://www.slide-and-scroll.org/
+
+Copyright 2018 SLIDE and SCROLL by Daniel Friedrich
+Released under the MIT license
+
+@author: Daniel Friedrich
+Date: 01.08.2018
+*/
+
+
+/*
+TABLE OF CONTENTS
+    I.   Layout
+    II.  Content
+    III. 
+*/
+
 $(document).ready(function () {
+
+    /*
+    _________________________________________________________________
+      
+    I. Layout
+    _________________________________________________________________
+    */
+
+
+
+    /*
+    _________________________________________________________________
+      
+    II. Content
+    _________________________________________________________________
+    */
+
+    /*
+    --------------------------------
+        Elements
+    --------------------------------
+    */
+
+    /*
+     *  Media
+     */
 
     // image format: landscape 16:9
     $(window).on('resize', function () {
@@ -44,6 +79,32 @@ $(document).ready(function () {
             return $(this).outerWidth();
         });
     }).resize();
+
+
+
+    /*
+    --------------------------------
+        Components
+    --------------------------------
+    */
+
+    /*
+     *  Accordions
+     */
+
+    // Initializes the icon "up" when a bellows is shown.
+    $('.SNSFFW-Accordion_BellowsShow').find('.SNSFFW-Accordion_BellowsHeader--Icon').addClass('SNSFFW-Accordion_BellowsHeader--IconUp');
+
+    // If the bellows header is clicked, than ...
+    $('.SNSFFW-Accordion_BellowsHeader').click(function () {
+        // ... all bellows bodies disappear and all icons show "Down" (except this bellows), ...
+        $(this).parent('.SNSFFW-Accordion_Bellows').siblings().find('.SNSFFW-Accordion_BellowsBody').slideUp(250);
+        $(this).parent('.SNSFFW-Accordion_Bellows').siblings().find('.SNSFFW-Accordion_BellowsHeader--Icon').removeClass('SNSFFW-Accordion_BellowsHeader--IconUp');
+
+        // ... bellows body toggles between appear and disappear and icon toggles between "Up" and "Down" (only this bellows).
+        $(this).children('.SNSFFW-Accordion_BellowsHeader--Icon').toggleClass('SNSFFW-Accordion_BellowsHeader--IconUp');
+        $(this).next('.SNSFFW-Accordion_BellowsBody').slideToggle(250);
+    });
 
 
 
